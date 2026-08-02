a73e40a2df0a1210b805e1b9c58e2e977b4dc453
Zhen Bang <zbliow12570zb@gmail.com>
2026-08-02 14:06:14 +0800

    Fix CIMB intent

--- resources/js/Pages/Receipts/Show.vue
+++ resources/js/Pages/Receipts/Show.vue
@@ -463,7 +463,7 @@ const appStoreLinks = {
     name: 'CIMB OCTO',
     scheme: 'cimbocto://',
     intentScheme: 'cimbocto',
-    intentPackage: 'com.cimb.octo',
+    intentPackage: 'com.cimb.cimbocto',
     playStore: 'https://play.google.com/store/apps/details?id=com.cimb.cimbocto',
     appStore: 'https://apps.apple.com/app/cimb-octo-my/id1608670830',
     color: 'bg-rose-600 hover:bg-rose-700 text-white',
@@ -510,7 +510,7 @@ const getStoreHref = (appName) => {
   const target = appStoreLinks[appName];
   if (!target) return '#';
   if (isIOS) return target.appStore;
-  if (isAndroid && target.intentScheme && target.intentPackage && appName !== 'tng' && appName !== 'mae' && appName !== 'cimb') {
+  if (isAndroid && target.intentScheme && target.intentPackage && appName !== 'tng' && appName !== 'mae') {
     return `intent://open#Intent;scheme=${target.intentScheme};package=${target.intentPackage};end`;
   }
   return target.playStore;
@@ -521,7 +521,7 @@ const openEWalletApp = (event, appName) => {
   if (!target) return;
   if (isIOS) {
     window.location.href = target.scheme;
-  } else if (isAndroid && (appName === 'tng' || appName === 'mae' || appName === 'cimb')) {
+  } else if (isAndroid && (appName === 'tng' || appName === 'mae')) {
     window.location.href = target.scheme;
   }
 };
